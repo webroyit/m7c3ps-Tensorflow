1730dcd115b84664af44198f075e476b8029e8aa
royweb <xytank@gmail.com>
2019-04-05 14:35:11 -0400

    Store the data from CSV

---
+++ index.js
@@ -0,0 +1,13 @@
+const loadCSV = require('./load-csv');
+
+// shuffle -  randonize the data
+// splitTest - get number of data
+// dataColumns - get data base on the columns
+let { features, labels, testFeatures, testLabels } = loadCSV('kc_house_data.csv', {
+    shuffle: true,
+    splitTest: 10,
+    dataColumns: ['lat', 'long'],
+    labelColumns: ['price']
+});
+
+console.log(features);
